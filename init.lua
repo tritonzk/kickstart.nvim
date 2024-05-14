6297cc6d3d4b17f1ae938a619b0f2c9008752796
tritonzk <boxyox0x@duck.com>
2024-05-14 20:32:36 +0200

    added python autocmd

--- init.lua
+++ init.lua
@@ -57,6 +57,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- create keymaps for running python
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = 'python',
+  command = "map <buffer> <F6> :w<CR>:exec '!python' shellescape(@%,1)<CR>",
+})
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = 'python',
+  command = "imap <buffer> <F6> :w<CR>:exec '!python' shellescape(@%,1)<CR>",
+})
+
 --  NOTE: [[ Install `lazy.nvim` plugin manager ]] See `:help lazy.nvim.txt`
 --  or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
