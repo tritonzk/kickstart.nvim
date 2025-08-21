580e1df7702fb0cc927ff5603cbdef0c25d1bf63
tritonzk <boxyox0x@duck.com>
2025-08-21 08:52:41 +0200

    more arch compat
    cond jit windows

--- lua/custom/plugins/lsp_config.lua
+++ lua/custom/plugins/lsp_config.lua
@@ -1,18 +1,20 @@
-local on_attach = require('plugins.configs.lspconfig').on_attach
-local capabilities = require('plugins.configs.lspconfig').capabilites
-local lspconfig = require 'lspconfig'
-local util = require 'lspconfig/util'
+if jit.os == 'Windows' then
+  local on_attach = require('plugins.configs.lspconfig').on_attach
+  local capabilities = require('plugins.configs.lspconfig').capabilites
+  local lspconfig = require 'lspconfig'
+  local util = require 'lspconfig/util'
 
-lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { 'rust' },
-  root_dir = util.root_pattern 'Cargo.toml',
-  settings = {
-    ['rust_analyzer'] = {
-      cargo = {
-        allFeatures = true,
+  lspconfig.rust_analyzer.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { 'rust' },
+    root_dir = util.root_pattern 'Cargo.toml',
+    settings = {
+      ['rust_analyzer'] = {
+        cargo = {
+          allFeatures = true,
+        },
       },
     },
-  },
-}
+  }
+end
--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,5 +1,6 @@
 return {
   'epwalsh/obsidian.nvim',
+  cond = jit.os == 'Windows',
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = 'markdown',
